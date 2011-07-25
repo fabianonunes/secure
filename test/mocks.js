a3b3c5419012f661384adb3f8758d624e41d391c
Fabiano Nunes <fabiano@nunes.me>
2011-07-25 10:09:22 -0300

    userstore mock returns a promise

--- test/mocks.js
+++ test/mocks.js
@@ -1,19 +1,24 @@
+var jdefer = require('jdefer');
+
 module.exports.mockStore = {
-	findByUsername : function()	{
-		var user = {
-			username: 'falha404'
-			, salt: 'salt'
-			, auth: 'ae73218f48ae039148ba5a64072e90114d5039c4953bf43e7a4294794c6a222b'
-		};
-		return {
-			done : function(arg){
-				arg(user);
-				return this;
-			},
-			fail : function(arg) {
-				arg(user);
-				return this;
+	findByUsername : function(username)	{
+
+		var d = jdefer();
+
+		var users = {
+			'falha404' : {
+				username: 'falha404'
+				, salt: 'salt'
+				, auth: 'ae73218f48ae039148ba5a64072e90114d5039c4953bf43e7a4294794c6a222b'
 			}
 		};
+
+		var user = users[username];
+
+		user ? d.resolve(user) : d.reject(new Error('user not exists'));
+
+		return d.promise();
+
 	}
+	
 };
